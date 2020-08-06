b426adf5e181311ac146e49bb238ecc236f5a9a1
Ridwan Oseni <ridwanoseni101@gmail.com>
2020-08-05 17:30:22 -0700

    weather-icon displays

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ window.addEventListener('load', () => {
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimeZone = document.querySelector('.location-timezone');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let locationIcon = document.querySelector('.weather-icon')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             long = position.coords.longitude;
-            lat = position.coords.latitude
+            lat = position.coords.latitude;
+            
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2a551674edbf192f1db464248ea597ec`
+            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=2a551674edbf192f1db464248ea597ec`
+
+
+            
 
             fetch(api)
                 .then(response => {
@@ -18,23 +23,23 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
-                    console.log(data.weather[0].description);
-                    console.log(data.timezone);
+                    console.log(data.current.weather[0].description);
+                   
 
-                    const {temp} = data.main;
-                    const {description} = data.weather[0];
+                    const {temp} = data.current;
+                    const {description, icon} = data.current.weather[0];
                     const {timezone} = data;
+                    
+
                     // Set DOM Elements from the API
                     temperatureDegree.textContent = temp;
                     temperatureDescription.textContent = description;
-                    locationTimeZone.textContent = timezone;
+                    locationTimezone.textContent = timezone;
+                    locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
+
                 });
         });
     }else{
         h1.textContent = "Hey this is not working"
-    }
-
-    function setIcons(icon, iconID) {
-        const skycons = new Skycons({"color": "white"});
     }
 });      
